4208da0259354774f8b615bc4055deb2d525fd6a
parsiaprint <chap.parsia1@gmail.com>
2025-07-06 11:54:45 +0330

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,16 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "PASTE_YOUR_BOT_TOKEN_HERE"
+
+logging.basicConfig(level=logging.INFO)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام! ربات با موفقیت روی Render اجرا شده :)")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+if __name__ == "__main__":
+    app.run_polling()
